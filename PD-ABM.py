d05792fb5b01b4f137541b8d217ea2b90e6860ea
Zach Fulker <zachfulker@gmail.com>
2018-09-26 11:45:27 -0400

    PD-ABM

--- PD-ABM.py
+++ PD-ABM.py
@@ -1,19 +1,16 @@
 import random
+from itertools import accumulate
+import numpy as np
 
 
 # create class for agents
 class Player:
-    def __init__(self, cooperate, defect):
+    def __init__(self, cooperate, defect, matrix_size): # !!!! add personal adj_matrix
         self.cooperate = cooperate
         self.defect = defect
         self.win_total = 0
-
-    def update(self, opponent_defect, round_winnings):
-        if opponent_defect:
-            self.defect = self.defect + 1
-        else:
-            self.cooperate = self.cooperate + 1
-        self.win_total = self.win_total + round_winnings
+        self.adj_matrix = np.ones((self.matrix_size, self.matrix_size), dtype=float) # !!! what is self usage here
+        np.fill_diagonal(self.adj_matrix, 0)
 
     def choose_strategy(self):
         defect_ratio = self.defect / (self.cooperate + self.defect)
@@ -23,6 +20,12 @@ class Player:
             defect = False
         return defect
 
+    def update(self, defect, round_winnings): # !!!!! update personal adj_matrix
+        if defect:
+            self.defect = self.defect + round_winnings
+        else:
+            self.cooperate = self.cooperate + round_winnings
+        self.win_total = self.win_total + round_winnings
 
 # each agent selects randomly (with equal likelyhood) one other agent to play for their turn in the round), total games in each round is number of agents
 def run_game(agents, payoffs, rounds):
@@ -35,31 +38,34 @@ def run_game(agents, payoffs, rounds):
             agent_defect = agent.choose_strategy()
             agent_winnings = payoffs[agent_defect][opponent_defect]
             opponent_winnings = payoffs[opponent_defect][agent_defect]
-            agent.update(opponent_defect, agent_winnings)
-            opponent.update(agent_defect, opponent_winnings)
+            agent.update(agent_defect, agent_winnings)
+            opponent.update(opponent_defect, opponent_winnings)
     return agents
 
 #
-def run_game_network(agents, payoffs, rounds, adj_matrix):
+def run_game_network(agents, payoffs, rounds):  # !!!!! auto create agent objects
+    # begin game
     for round in list(range(rounds)):
-        for i in range(len(agents)): ####
-            prob_sum = 0
-            for opponent_index, item in enumerate(adj_matrix[i]):
-                prob_sum = item + prob_sum
-                if random.random(0, 1) <= prob_sum:
+        # each agent chooses an opponent in each round
+        random_num = random.uniform(0, sum(adj_matrix[i]))
+        for i in range(len(agents)):
+            for opponent_index, item in enumerate(accumulate(adj_matrix[i])):
+                if random_num <= item:
                     break
+            # agents pick strategy and then update based on game results
             opponent_defect = agents[opponent_index].choose_strategy()
             agent_defect = agents[i].choose_strategy()
             agent_winnings = payoffs[agent_defect][opponent_defect]
             opponent_winnings = payoffs[opponent_defect][agent_defect]
-            agents[i].update(opponent_defect, agent_winnings)
-            agents[opponent_index].update(agent_defect, opponent_winnings)
+            agents[i].update(agent_defect, agent_winnings)
+            agents[opponent_index].update(opponent_defect, opponent_winnings)
     return agents
 
+
 # Game Setup and execution
-payoffs = [[3, 0], [2, 1]]
-adj_matrix = [[0,.5,0,.5,0,0], [.5,0,.5,0,0,0], [0,.5,0,0,0,.5], [.5,0,0,0,.5,0], [0,0,0,.5,0,.5], [0,0,.5,0,.5,0]] #look for multinomial choice option
-p1 = Player(1, 1)
+payoffs = [[1, 0], [(2/3), (1/3)]]
+#adj_matrix = [[0,.5,0,.5,0,0], [.5,0,.5,0,0,0], [0,.5,0,0,0,.5], [.5,0,0,0,.5,0], [0,0,0,.5,0,.5], [0,0,.5,0,.5,0]] #look for multinomial choice option
+p1 = Player(1, 1,1)
 p2 = Player(1, 1)
 p3 = Player(1, 1)
 p4 = Player(1, 1)
@@ -73,8 +79,6 @@ for i in list(range(len(agents))):
     print(results[i].defect)
     print(results[i].cooperate)
 
-#switch to payoff reinforcement
 #reinforce link weights in same way (so strategy and links coevolve)
-#Start with complete networks that evolves based on payoffs (np.ones - np.diagonal zeros)
 #try to get sense of how system is evolving, Change payoffs see what happens
 #statistic that shows proportion of how likely  to cooperate over time and plot it
